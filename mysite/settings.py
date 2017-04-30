b4c7cc93e753e7af08c81224666a1022277178f0
izzysmith <isobel@bridesmaid.org>
2017-04-30 01:00:24 +0100

    changed settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,12 +122,12 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+#DATABASES['default'] = dj_database_url.config()
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'bd.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
